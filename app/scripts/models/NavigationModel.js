d14c11c8612062b152d5017cbdba72cdc8bf5447
Jordan S. Jones <Jordan.Jones@software.dell.com>
2014-12-01 13:06:20 -0700

    Style tweaks.

--- app/scripts/models/NavigationModel.js
+++ app/scripts/models/NavigationModel.js
@@ -4,14 +4,14 @@ module.exports = {
     baseName: 'Overview',
     routes: [
         {route: 'Overview', name: 'Overview', title: 'Overview'},
+        {route: 'Summary', name: 'Summary', title: 'Summary'},
         {route: 'Result', name: 'WebApiSqlClient', title: 'WebApi SqlClient'},
         {route: 'Result', name: 'ODataEntityFramework', title: 'OData EF (DB First)'},
         {route: 'Result', name: 'ODataEntityFrameworkCodeFirst', title: 'OData EF (Code First)'},
         {route: 'Result', name: 'ODataLinq2Db', title: 'OData Linq2Db'},
         {route: 'Result', name: 'WebApiEntityFramework', title: 'WebApi EF (DB First)'},
         {route: 'Result', name: 'WebApiEntityFrameworkCodeFirst', title: 'WebApi EF (Code First)'},
-        {route: 'Result', name: 'WebApiLinq2Db', title: 'WebApi Linq2Db'},
-        {route: 'Summary', name: 'Summary', title: 'Summary'}
+        {route: 'Result', name: 'WebApiLinq2Db', title: 'WebApi Linq2Db'}
     ],
 
     getResultRoutes: function () {
